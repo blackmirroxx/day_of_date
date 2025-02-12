c6072eaa35b04a1c41084e44df8320706c9019ca
Olivier Mutschler -- ricardo <olivier.mutschler@gmail.com>
2025-02-12 18:59:32 +0100

    tell me the day of date in haskell

---
+++ date.hs
@@ -0,0 +1,13 @@
+import System.Environment
+import Data.Time
+import Data.Time.Format
+import Control.Monad
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let dateString = head args
+    let date = parseTimeM True defaultTimeLocale "%Y%m%d" dateString :: Maybe UTCTime
+    case date of
+        Just d -> putStrLn $ formatTime defaultTimeLocale "%B %d, %Y :: %A" d
+        Nothing -> putStrLn "Howto run : ./date <YYYYMMDD>"
